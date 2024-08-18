6c6e7ad5c458f15b21d38210ddbaed9794b84153
JoaoTony <joaojosetony2016@gmail.com>
2024-08-18 20:16:22 +0100

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by João Tony on 18/08/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
